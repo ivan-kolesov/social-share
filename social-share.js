b5d7d934d14ff4130ef0cb532f5d0a0f7cd00317
Ivan <ivan.kolesov@gmail.com>
2015-02-04 03:16:05 +0300

    share

--- social-share.js
+++ social-share.js
@@ -2,6 +2,10 @@
 var Sharing = {
     pageuri: App.rootUrl,
     facebook: function (options, callback) {
+        callback = function() {
+            this.fbCount();
+        }.bind(this);
+
         return this._share("//www.facebook.com/sharer.php", {u: options.url}, callback);
     },
     vkontakte: function (options, callback) {
@@ -18,7 +22,7 @@ var Sharing = {
     },
     fbCount: function () {
         return $.getJSON("//graph.facebook.com/?id=" + this.pageuri, function (response) {
-            return $("@fb_count").text(response.shares);
+            return $(".js-fb_count").text(response.shares);
         })
     },
     vkCount: function () {
@@ -30,13 +34,13 @@ var Sharing = {
         }, $.getJSON("//vk.com/share.php?act=count&index=1&url=" + this.pageuri + "&format=json&callback=?");
     },
     _share: function (baseUrl, params, callback) {
-        var self = this;
         var url = baseUrl + "?" + $.param(params);
         this.window = window.open(url, "Sharing", "width=740,height=440");
+
         clearInterval(this.interval);
         return this.interval = setInterval(function () {
-            return self.window.closed && (clearInterval(self.interval), callback) ? callback() : void 0;
-        }, 500);
+            return this.window.closed && (clearInterval(this.interval), callback) ? callback() : void 0;
+        }.bind(this), 500);
     }
 };
 
@@ -44,12 +48,14 @@ $(function () {
     window.App = window.App || {};
 
     $(document).on("click", ".js-share", function () {
-        var self = $(this), network = self.data("network");
-        return "function" == typeof Sharing[network] ? Sharing[network]({
-            url: location.href,
-            title: App.metaTags.title,
-            message: App.metaTags.description,
-            image: App.metaTags.vkontakte_image
-        }) : void 0;
+        var network = $(this).data("network");
+        if ("function" == typeof Sharing[network]) {
+            Sharing[network]({
+                url: location.href,
+                title: App.metaTags.title,
+                message: App.metaTags.description,
+                image: App.metaTags.vkontakte_image
+            });
+        }
     });
 });
